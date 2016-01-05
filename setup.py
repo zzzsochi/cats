32e7e03b3ca8d24fc67625c2aacb4afed175334b
Alexander Zelenyak <zzz.sochi@gmail.com>
2016-01-06 01:40:53 +0300

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='cats',
     description='More cats to your life!',
-    version='0.1.0',
+    version='0.1.1',
     classifiers=[
         "License :: OSI Approved :: BSD License",
         "Operating System :: POSIX",
