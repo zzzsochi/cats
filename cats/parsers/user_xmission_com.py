afa03e663622606dfc703b63122ac82319b5d485
Alexander Zelenyak <zzz.sochi@gmail.com>
2016-01-07 15:54:42 +0300

    Same fixes for user.xmission.com

--- cats/parsers/user_xmission_com.py
+++ cats/parsers/user_xmission_com.py
@@ -17,10 +17,10 @@ async def parse(loop):
         tt = tr.tt
         cat = (str(tt)
                .replace('\xa0', ' ')
-               .replace('\n', '')
+               .replace('\n', ' ')
                .replace('<br>', '\n')
                .replace('</br>', '')
-               .replace('<tt>', '')
+               .replace('<tt>', ' ')
                .replace('</tt>', '')
                .replace('<b>', '')
                .replace('</b>', '')
@@ -30,6 +30,19 @@ async def parse(loop):
                .replace('&lt;', '<')
                .replace('&amp;', '&')
                )
+
+        def strip_first(lines):
+            res = lines.copy()
+            for line in lines:
+                if not line.strip():
+                    del res[0]
+                else:
+                    break
+
+            return res
+
+        cat = '\n'.join(strip_first(cat.split('\n')))
+        cat = '\n'.join(reversed(strip_first(list(reversed(cat.split('\n'))))))
 
         if len(cat.strip()) > 5:
             cats.append(cat)
